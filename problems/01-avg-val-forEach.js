d12ae5aa530835d95403df838431b32142977b5c
jayray3991 <james.r45@aol.com>
2024-03-30 16:23:09 -0400

    01 problem solved

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -14,9 +14,26 @@ console.log(avgVal([])); // null
 */
 
 let avgVal = function(arr) {
-    // Your code here 
-};
+    let sum = 0;
+    if (arr.length === 0) return null
+    arr.forEach((num) => {
 
+      if (arr.length > 0) {
+        sum += num;
+
+      }
+      //else {
+
+     // return null;
+
+     // }
+
+    })
+    return sum / arr.length
+};
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
